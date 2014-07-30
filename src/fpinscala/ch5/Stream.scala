d06318b4659e23d3b9bfe5de3afad6ac4cde7757
mikhnevich <overloading12>
2014-07-30 15:50:45 -0400

    ch5

--- src/fpinscala/ch5/Stream.scala
+++ src/fpinscala/ch5/Stream.scala
@@ -1,13 +1,39 @@
 package fpinscala.ch5
 
 
-sealed trait Stream[+A]
+sealed trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+
+  def toList: List[A] = this match {
+    case Empty => List()
+    case Cons(h, t) => h() :: t().toList
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Empty => Empty
+    case Cons(h, _) if n == 0 => Stream.cons(h(), Empty)
+    case Cons(h, t) => Stream.cons(h(), t().take(n - 1))
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Empty => Empty
+    case Cons(h, t) if n == 0 => this
+    case Cons(h, t) => t().drop(n - 1)
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = ???
+}
 
 case object Empty extends Stream[Nothing]
 
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
+
 object Stream {
+
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
     lazy val head = hd
     lazy val tail = tl
