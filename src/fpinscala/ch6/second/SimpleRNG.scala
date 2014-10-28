5e98900a25db35041dc61dafe9f2db59af264514
mikhnevich <overloading12>
2014-10-28 17:10:55 -0400

    fp in scala

--- src/fpinscala/ch6/second/SimpleRNG.scala
+++ src/fpinscala/ch6/second/SimpleRNG.scala
@@ -70,6 +70,7 @@ case class SimpleRNG(seed: Long) extends RNG {
     (f(a), r)
   }
 
+
   def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
 
   // 6.5
@@ -111,10 +112,10 @@ case class SimpleRNG(seed: Long) extends RNG {
     g(a)(r)
   }
 
-  def nonNegativeLessThan_flatmap(n: Int): Rand[Int] = {
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
     flatMap(nonNegativeInt) { i =>
       val mod = i % n
-      if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan_flatmap(n)
+      if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
     }
   }
 
@@ -129,4 +130,82 @@ case class SimpleRNG(seed: Long) extends RNG {
     flatMap(ra)(a => map(rb)(b => f(a, b)))
 
 
+  def rollDie: Rand[Int] = map(nonNegativeLessThan(6))(_ + 1)
+
 }
+
+case class State[S, +A](run: S => (A, S)) {
+  // 6.10
+  def map[B](f: A => B): State[S, B] =
+    flatMap(s => State.unit(f(s)))
+
+  // 6.10
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(
+    s => {
+      val (a, s1) = run(s)
+      f(a).run(s1)
+    }
+  )
+
+  // 6.10
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a, b)))
+
+}
+
+
+object State {
+  // 6.10
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+
+  // 6.10
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.foldRight(unit[S, List[A]](List()))((f, acc) => f.map2(acc)(_ :: _))
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
+
+}
+
+// EXERCISE 6.11
+// Hard: To gain experience with the use of State, implement a finite state automaton
+// that models a simple candy dispenser. The machine has two types of input: you can
+// insert a coin, or you can turn the knob to dispense candy. It can be in one of two
+// states: locked or unlocked. It also tracks how many candies are left and how many
+// coins it contains.
+sealed trait Input
+
+case object Coin extends Input
+
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+object Candy {
+
+  import State._
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    val k = sequence(inputs.map(i => modify((s: Machine) => (i, s) match {
+      case (_, Machine(_, 0, _)) => s
+      case (Coin, Machine(false, _, _)) => s
+      case (Turn, Machine(true, _, _)) => s
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(true, candy - 1, coin)
+    })))
+  for {
+    _ <- k
+    s <- get
+  } yield (s.coins, s.candies)
+}
+
+}
+
